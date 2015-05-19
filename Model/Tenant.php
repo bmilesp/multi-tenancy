faed912376b5fbbd1543e20a0f332061c94911ab
bp <bmilesp@gmail.com>
2015-05-19 14:57:26 -0400

    db mod

--- Model/Tenant.php
+++ Model/Tenant.php
@@ -2,9 +2,11 @@
 App::uses('AppModel', 'Model');
 class Tenant extends AppModel {
 
+	public $useDbConfig = 'tenants';
 
 	public function domainExists($domain){
 		if($this->findByDomain($domain)){
+			
 			return $domain;
 		}
 		return false;
