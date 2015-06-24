3af6e0e8dbd526132c6040cf83f79ccf5342a9a0
bp <bmilesp@gmail.com>
2015-06-24 13:35:54 -0400

    added more

--- Model/Tenant.php
+++ Model/Tenant.php
@@ -2,6 +2,7 @@
 App::uses('AppModel', 'Model');
 class Tenant extends AppModel {
 
+	public $useDbConfig = 'tenants';
 
 	public function domainExists($domain){
 		if($this->findByDomain($domain)){
--- config.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-Configure::write('Domain.Master', 'u');
-Configure::write('Domain.DefaultRoute', array('plugin' => 'multi_tenancy', 'controller' => 'sites', 'action' => 'add'));
-
-?>
